a5ff4bef252be80819c8ef13ffadde86d6aaf0f3
Killian Belliot <belliotkillian@icloud.com>
2023-03-29 16:11:24 +0200

    route details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './component/details/details.component';
 import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
-{path: '', component: HomeComponent}
+{path: '', component: HomeComponent},
+{path: 'details', component: DetailsComponent}
 ];
 
 @NgModule({
