663d0988ff6bb569e7d2c7521038bf84539e6e28
Killian Belliot <belliotkillian@icloud.com>
2023-04-06 16:40:14 +0200

    details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiSushi:'https://floppy-parrots-crash-92-184-117-118.loca.lt/api/boxes/',
-    pathImage: 'https://floppy-parrots-crash-92-184-117-118.loca.lt/api/image/'
+    apiSushi:'https://famous-parts-yell-92-184-117-118.loca.lt/api/boxes/',
+    pathImage: 'https://famous-parts-yell-92-184-117-118.loca.lt/api/image/'
 };
